48e7d2051513f5fe2bdf4833de58a225fb82a462
lyrain2001 <yliu217@u.rochester.edu>
2023-12-05 15:36:29 -0500

    data description generation

---
+++ src/description.py
@@ -0,0 +1,171 @@
+import tiktoken
+import openai
+
+class AutoDescription:
+    def __init__(self, openai_key, context):
+        self.context = context
+        # self.initial_description, self.description_PQ, self.potential_query = self.generate_description(self.context)
+        openai.api_key = (openai_key)
+
+    def get_context(self):
+        return self.context
+
+    def get_initial_description(self):
+        return self.initial_description
+    
+    def get_description_PQ(self):
+        return self.description_PQ
+    
+    def get_potential_query(self):
+        return self.potential_query
+            
+    def num_tokens_from_string(self, string, encoding_name="gpt-3.5-turbo"):
+        encoding = tiktoken.encoding_for_model(encoding_name)
+        num_tokens = len(encoding.encode(string))
+        return num_tokens
+    
+    def generate_description_profiler(self, context, model="gpt-3.5-turbo"):
+        print("Generating description with profiler...")
+        sample_profiler = self.generate_description_initial(context, model)
+        print("Generating description with template...")
+        template = self.generate_description_template(context, model)
+        return sample_profiler, template
+    
+    def generate_description_samples(self, context, model="gpt-3.5-turbo"):
+        print("Generating description with samples...")
+        sample = self.generate_description_samples(context, model)
+        return sample
+        
+    # def generate_description(self, context, model="gpt-3.5-turbo"):
+    #     initial_description_content = self.generate_description_initial(context, model)
+    #     potential_query_content = self.generate_potential_query(context, model)
+    #     description_PQ_content = self.generate_description_potential_query(initial_description_content, potential_query_content, model)
+    #     return initial_description_content, description_PQ_content, potential_query_content
+
+    def generate_description_initial(self, context, model):
+        description = openai.ChatCompletion.create(
+            model=model,
+            messages=[
+                    {
+                        "role": "system", 
+                        "content": "You are an assistant for a dataset search engine.\
+                                    Your goal is to increase the performance of this dataset search engine for keyword queries."},
+                    {
+                        "role": "user", 
+                        "content": "Answer the questions while using the input and context.\
+                                    The input are the hearders, a random sample, and profiler result of the large dataset. \
+                                    The context are.\n"+context+"\
+                                Question:Describe the dataset in sentences.\nAnswer:"},
+                ],
+            temperature=0.3)
+        description_content = description.choices[0]['message']['content']
+        return description_content
+    
+    def generate_description_samples(self, context, model):
+        description = openai.ChatCompletion.create(
+            model=model,
+            messages=[
+                    {
+                        "role": "system", 
+                        "content": "You are an assistant for a dataset search engine.\
+                                    Your goal is to increase the performance of this dataset search engine for keyword queries."},
+                    {
+                        "role": "user", 
+                        "content": "Answer the questions while using the input and context.\
+                                    The input are the hearders and a random sample of the large dataset. \
+                                    The context are.\n"+context+"\
+                                Question:Describe the dataset in sentences.\nAnswer:"},
+                ],
+            temperature=0.3)
+        description_content = description.choices[0]['message']['content']
+        return description_content
+    
+    def generate_description_template(self, context, model):
+        description = openai.ChatCompletion.create(
+            model=model,
+            messages=[
+                    {
+                        "role": "system", 
+                        "content": "You are an assistant for a dataset search engine.\
+                                    Your goal is to increase the performance of this dataset search engine for keyword queries."},
+                    {
+                        "role": "user", 
+                        "content": "Answer the questions while using the input and context.\
+                                    The input are the hearders, a random sample, and profiler result of the large dataset. \
+                                    The context are.\n"+context+"\
+                                    The instructions are:\
+                                    1. Describe the dataset in one sentence?\
+                                    2. What does the dataset look like?\
+                                    3. Can you group the headers?\
+                                    4. What are the value types and value ranges for the most important headers?\
+                                    5. Where is the data from?\
+                                    6. In what format or in what way does the dataset mention time?\
+                                    7. In what format or in what way does the dataset mention location?\
+                                    8. Is there anything unclear about the data, or do you have reason to doubt the quality?\
+                                    9. Is there anything that you can point out or analyse in more detail?\
+                                Question:Describe the dataset answering the nine questions above in one compelete and coherentparagraph.\nAnswer:"},
+                ],
+            temperature=0.3)
+        description_content = description.choices[0]['message']['content']
+        return description_content
+    
+    
+
+    # def generate_potential_query(self, context, model="gpt-3.5-turbo"):
+    #     description = openai.ChatCompletion.create(
+    #         model=model,
+    #         messages=[
+    #                 {
+    #                     "role": "system", 
+    #                     "content": "You are an assistant for an online dataset search engine.\
+    #                                 Your goal is to increase the performance of this dataset search engine for keyword queries."},
+    #                 {
+    #                     "role": "user", 
+    #                     "content": "Answer the questions while using the input and context.\
+    #                                 The input is a random sample of the large dataset. \
+    #                                 The context describes what the headers of the dataset are.\n"+context+"\
+    #                             Question: What can this dataset be used for? Please output top 3 keyword queries where users want to find this data\nAnswer:"},
+    #             ],
+    #         temperature=0.6)
+    #     description_content = description.choices[0]['message']['content']
+    #     return description_content
+
+    # def generate_description_potential_query(self, initial_description_content, potential_query_content, model="gpt-3.5-turbo"):
+    #     description = openai.ChatCompletion.create(
+    #         model=model,
+    #         messages=[
+    #                 {
+    #                     "role": "system", 
+    #                     "content": "You are an assistant for an online dataset search engine.\
+    #                                 Your goal is to increase the performance of this dataset search engine for keyword queries."},
+    #                 {
+    #                     "role": "user", 
+    #                     "content": "Answer the questions while using the following information.\
+    #                                 The description is about a dataset: "+initial_description_content+"\
+    #                                 The suggested queries are top keyword queries that users want to find this dataset.\n"+potential_query_content+"\
+    #                             Question: Based on the suggested keyword queries. Can you improve the current dataset description to cover these keyword queries? \
+    #                                     Only new dataset description is needed in the response.\nAnswer:"},
+    #             ],
+    #         temperature=0.9)
+    #     description_content = description.choices[0]['message']['content']
+    #     return description_content
+    
+    # def generate_description_combined(self, original_description, description_PQ_content, model="gpt-3.5-turbo", temperature=0.9):
+    #     description = openai.ChatCompletion.create(
+    #         model=model,
+    #         messages=[
+    #                 {
+    #                     "role": "system", 
+    #                     "content": "You are an assistant for an online dataset search engine.\
+    #                                 Your goal is to increase the performance of this dataset search engine for keyword queries."},
+    #                 {
+    #                     "role": "user", 
+    #                     "content": "Answer the questions while using the following information.\
+    #                                 This is the original description about this dataset: "+original_description+"\
+    #                                 This is a description based on the suggested keyword queries on this dataset.\n"+description_PQ_content+"\
+    #                             Question: Based on these two descriptions. Can you generate a new description by summarizing and highlighting the important parts of these two versions of descriptions? \
+    #                                     Only return the new description.\nAnswer:"},
+    #             ],
+    #         temperature=temperature)
+    #     description_content = description.choices[0]['message']['content']
+    #     return description_content
--- src/generate_description.py
+++ src/generate_description.py
@@ -1,222 +1,52 @@
-import tiktoken
-import datamart_profiler
-import openai
-from datetime import datetime
+import argparse
+try:
+    from .description import *
+except ImportError: 
+    from description import *
 
-class AutoDescription:
-    def __init__(self, openai_key, sample_size):
-        self.sample_size = sample_size
-        self.context = None
-        self.contextArr = None
-        # self.initial_description, self.description_PQ, self.potential_query = self.generate_description(self.context)
-        openai.api_key = (openai_key)
+class DescriptionGenerator:
+    def __init__(self, openai_key, task_name):
+        self.openai_key = openai_key
+        self.task_name = task_name
 
-    def get_context(self):
-        return self.context
+    def read_contexts(self, filename):
+        with open(filename, 'r') as file:
+            lines = file.readlines()
+        return [line.strip().split(' ', 1) for line in lines if line.strip()]
 
-    def get_initial_description(self):
-        return self.initial_description
-    
-    def get_description_PQ(self):
-        return self.description_PQ
-    
-    def get_potential_query(self):
-        return self.potential_query
-            
-    def num_tokens_from_string(self, string, encoding_name="gpt-3.5-turbo"):
-        encoding = tiktoken.encoding_for_model(encoding_name)
-        num_tokens = len(encoding.encode(string))
-        return num_tokens
+    def generate_and_write_descriptions(self):
+        profiler_filename = f'../samples/{self.task_name}_sample_profiler.txt'
+        sample_filename = f'../samples/{self.task_name}_sample.txt'
 
-    @staticmethod
-    def form_context(contextDict,sample):
-        result=""
+        profiler_contexts = self.read_contexts(profiler_filename)
+        sample_contexts = self.read_contexts(sample_filename)
 
-        temporal_columns = []
-        if 'temporal_coverage' in contextDict.keys():
-            for d in contextDict['temporal_coverage']:
-                temporal_columns += d['column_names']
-                result+="Column "+d['column_names'][0]+" is a time column with temporal resolution in "+d['temporal_resolution']+". "
-                result+="The temporal coverage is from "+d['ranges'][0]+" to "+d['ranges'][1]+". "
-                result+="\n"
-        print("temporal_columns", temporal_columns)
+        with open(f'../results/{self.task_name}_result_profiler.txt', 'w') as profiler_file, \
+            open(f'../results/{self.task_name}_result_sample.txt', 'w') as sample_file, \
+            open(f'../results/{self.task_name}_result_template.txt', 'w') as template_file:
 
-        for d in contextDict['columns']:
-            if d['name'] in temporal_columns:
-                continue
-            result+="Column "+d['name']+" is a "+d['structural_type']+". "
-            if('num_distinct_values' in d.keys()):
-                result+="There are "+str(d['num_distinct_values'])+" distinct values. "
-            if('coverage' in d.keys()):
-                result+="The range of values is from "+str(d['coverage'][0])+" to "+str(d['coverage'][1])+". "
-            if len(d['semantic_types'])>0:
-                result+="The semantic types are: "
-                for s in d['semantic_types']:
-                    result+=s+", "
-                result=result[:-2]
-                result+=". "
-            result+="\n"
-        
-        final = "Input: \n" + sample + "\n\n" + "Context: \n" + result
-        return final
+            for profiler_context in profiler_contexts:
+                print("Generating description with profiler for", profiler_context[0])
+                auto_desc = AutoDescription(self.openai_key, profiler_context[1])
+                description_profiler, description_template = auto_desc.generate_description_profiler(profiler_context[1])
+                profiler_file.write(f'{profiler_context[0]}\t{description_profiler}\n')
+                template_file.write(f'{profiler_context[0]}\t{description_template}\n')
 
+            for sample_context in sample_contexts:
+                print("Generating description with only samples for", sample_context[0])
+                auto_desc = AutoDescription(self.openai_key, sample_context[1])
+                description_sample = auto_desc.generate_description_samples(sample_context[1])
+                sample_file.write(f'{sample_context[0]}\t{description_sample}\n')
 
-    def build_context(self, df, sample_size=None):
-        if sample_size is None:
-            sample_size = self.sample_size
-        try:
-            contextArr = self.profiler(df)
-            self.contextArr = contextArr
-            if sample_size <= len(df):
-                df_sample = df.sample(sample_size)
-            else:
-                df_sample = df
-            sample = df_sample.to_csv(index=False)
+def main():
+    parser = argparse.ArgumentParser(description="Generate descriptions for datasets")
+    parser.add_argument('--task_name', type=str, default='task1', help='Task name (i.e. task1)')
 
-            context=self.form_context(contextArr,sample)
-            num_tokens = self.num_tokens_from_string(context)
+    args = parser.parse_args()
+    OPENAI_API_KEY = "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"
 
-            # Decrease the sample size iteratively until the number of tokens is less than or equal to 4000
-            while num_tokens > 4000:
-                sample_size -= 1
+    generator = DescriptionGenerator(OPENAI_API_KEY, args.task_name)
+    generator.generate_and_write_descriptions()
 
-                if sample_size <= len(df):
-                    df_sample = df.sample(sample_size)
-                else:
-                    df_sample = df
-                sample = df_sample.to_csv(index=False)
-
-                context = self.form_context(contextArr, sample)
-                num_tokens = self.num_tokens_from_string(context)
-        except Exception as e:
-            print(f"An exception occurred: {e}")
-            context = ""
-        self.context = context
-        return context
-        
-    @staticmethod
-    def profiler(data_frame):
-        metadata = datamart_profiler.process_dataset(data_frame)
-        contextDict = {}
-
-        contextArr=[]
-        for i in range(len(metadata['columns'])):
-            metDict = metadata['columns'][i]
-            cDict={'name':metDict['name']
-                    ,'structural_type':metDict['structural_type'],
-                    'semantic_types':metDict['semantic_types']}
-            if('num_distinct_values' in metDict.keys()):
-                cDict['num_distinct_values'] = metDict['num_distinct_values']
-            if('coverage' in metDict.keys()):
-                low=0
-                high=0
-                for i in range(len(metDict['coverage'])):
-                    if(metDict['coverage'][i]['range']['gte']<low):
-                        low=metDict['coverage'][i]['range']['gte']
-                    if(metDict['coverage'][i]['range']['lte']>high):
-                        high=metDict['coverage'][i]['range']['lte']
-                cDict['coverage'] = [low,high]
-            contextArr.append(cDict)
-        contextDict['columns']=contextArr
-
-        if 'temporal_coverage' in metadata.keys():
-            temporal_coverage = []
-            for i in range(len(metadata['temporal_coverage'])):
-                data = metadata['temporal_coverage'][i]
-                range_values = [entry['range']['gte'] for entry in data['ranges']] + [entry['range']['lte'] for entry in data['ranges']]
-                min_value = datetime.fromtimestamp(min(range_values)).strftime("%Y-%m-%d")
-                max_value = datetime.fromtimestamp(max(range_values)).strftime("%Y-%m-%d")
-                temporal_dict = {
-                    'column_names': data['column_names'],
-                    'temporal_resolution': data['temporal_resolution'],
-                    'ranges': [min_value, max_value]
-                }
-                temporal_coverage.append(temporal_dict)
-            contextDict['temporal_coverage'] = temporal_coverage
-
-        contextDict['attribute_keywords'] = metadata['attribute_keywords']
-        return contextDict
-
-    def generate_description(self, context, model="gpt-3.5-turbo"):
-        initial_description_content = self.generate_description_initial(context, model)
-        potential_query_content = self.generate_potential_query(context, model)
-        description_PQ_content = self.generate_description_potential_query(initial_description_content, potential_query_content, model)
-        return initial_description_content, description_PQ_content, potential_query_content
-
-    def generate_description_initial(self, context, model):
-        description = openai.ChatCompletion.create(
-            model=model,
-            messages=[
-                    {
-                        "role": "system", 
-                        "content": "You are an assistant for a dataset search engine.\
-                                    Your goal is to increase the performance of this dataset search engine for keyword queries."},
-                    {
-                        "role": "user", 
-                        "content": "Answer the questions while using the input and context.\
-                                    The input is a random sample of the large dataset. \
-                                    The context describes what the headers of the dataset are.\n"+context+"\
-                                Question:Describe the dataset in sentences.\nAnswer:"},
-                ],
-            temperature=0.3)
-        description_content = description.choices[0]['message']['content']
-        return description_content
-
-    def generate_potential_query(self, context, model="gpt-3.5-turbo"):
-        description = openai.ChatCompletion.create(
-            model=model,
-            messages=[
-                    {
-                        "role": "system", 
-                        "content": "You are an assistant for an online dataset search engine.\
-                                    Your goal is to increase the performance of this dataset search engine for keyword queries."},
-                    {
-                        "role": "user", 
-                        "content": "Answer the questions while using the input and context.\
-                                    The input is a random sample of the large dataset. \
-                                    The context describes what the headers of the dataset are.\n"+context+"\
-                                Question: What can this dataset be used for? Please output top 3 keyword queries where users want to find this data\nAnswer:"},
-                ],
-            temperature=0.6)
-        description_content = description.choices[0]['message']['content']
-        return description_content
-
-    def generate_description_potential_query(self, initial_description_content, potential_query_content, model="gpt-3.5-turbo"):
-        description = openai.ChatCompletion.create(
-            model=model,
-            messages=[
-                    {
-                        "role": "system", 
-                        "content": "You are an assistant for an online dataset search engine.\
-                                    Your goal is to increase the performance of this dataset search engine for keyword queries."},
-                    {
-                        "role": "user", 
-                        "content": "Answer the questions while using the following information.\
-                                    The description is about a dataset: "+initial_description_content+"\
-                                    The suggested queries are top keyword queries that users want to find this dataset.\n"+potential_query_content+"\
-                                Question: Based on the suggested keyword queries. Can you improve the current dataset description to cover these keyword queries? \
-                                        Only new dataset description is needed in the response.\nAnswer:"},
-                ],
-            temperature=0.9)
-        description_content = description.choices[0]['message']['content']
-        return description_content
-    
-    def generate_description_combined(self, original_description, description_PQ_content, model="gpt-3.5-turbo", temperature=0.9):
-        description = openai.ChatCompletion.create(
-            model=model,
-            messages=[
-                    {
-                        "role": "system", 
-                        "content": "You are an assistant for an online dataset search engine.\
-                                    Your goal is to increase the performance of this dataset search engine for keyword queries."},
-                    {
-                        "role": "user", 
-                        "content": "Answer the questions while using the following information.\
-                                    This is the original description about this dataset: "+original_description+"\
-                                    This is a description based on the suggested keyword queries on this dataset.\n"+description_PQ_content+"\
-                                Question: Based on these two descriptions. Can you generate a new description by summarizing and highlighting the important parts of these two versions of descriptions? \
-                                        Only return the new description.\nAnswer:"},
-                ],
-            temperature=temperature)
-        description_content = description.choices[0]['message']['content']
-        return description_content
+if __name__ == "__main__":
+    main()
