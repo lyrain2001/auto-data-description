dfa6da3c4f06cb5af0106fffdf16209f79e4b382
lyrain2001 <yliu217@u.rochester.edu>
2023-12-05 16:34:09 -0500

    add task2 samples

--- bash/profiler.sh
+++ bash/profiler.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-DATASETS_DIR="../../dataset/task1data"
-OUTPUT_FILE="../samples/task1_sample_profiler.txt"
+DATASETS_DIR="../../dataset/task2data"
+OUTPUT_FILE="../samples/task2_sample_profiler.txt"
 
 PYTHON_SCRIPT="../src/generate_context.py"
 
@@ -9,5 +9,5 @@ SAMPLE_SIZE=10
 
 python $PYTHON_SCRIPT --input_dir $DATASETS_DIR --output $OUTPUT_FILE --sample_size $SAMPLE_SIZE --profiler true
 
-OUTPUT_FILE="../samples/task1_sample.txt"
+OUTPUT_FILE="../samples/task2_sample.txt"
 python $PYTHON_SCRIPT --input_dir $DATASETS_DIR --output $OUTPUT_FILE --sample_size $SAMPLE_SIZE
