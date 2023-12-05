806621882c04029afa12a8b49c95cad777777d4c
lyrain2001 <yliu217@u.rochester.edu>
2023-12-05 14:01:27 -0500

    fix task1 samples

--- bash/task1profiler.sh
+++ bash/task1profiler.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
-# Define the directory containing the datasets and output file path
 DATASETS_DIR="../../dataset/task1data"
 OUTPUT_FILE="../samples/task1_sample.txt"
 
-# Path to your Python script
 PYTHON_SCRIPT="../src/generate_context.py"
 
-# Sample size
 SAMPLE_SIZE=10
 
-# Profiler
-PROFILER=False
-
-# Run the Python script
 python $PYTHON_SCRIPT --input_dir $DATASETS_DIR --output $OUTPUT_FILE --sample_size $SAMPLE_SIZE
